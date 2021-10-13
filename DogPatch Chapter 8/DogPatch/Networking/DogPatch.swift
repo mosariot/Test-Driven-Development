b9e6580eb60ef31ad9a00d2f04ab39c5767f411e
mosariot <mosariot@gmail.com>
2021-10-13 16:51:41 +0300

    Chapter 8 in the middle

--- DogPatch Chapter 8/DogPatch/Networking/DogPatch.swift
+++ DogPatch Chapter 8/DogPatch/Networking/DogPatch.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2019 Razeware LLC
-///
+/// Copyright (c) 2021 Razeware LLC
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,21 +27,34 @@
 /// THE SOFTWARE.
 
 import Foundation
-import XCTest
 
-protocol DecodableTestCase: AnyObject {
-  associatedtype T: Decodable
-  var dictionary: NSDictionary! { get set }
-  var sut: T! { get set }
-}
-extension DecodableTestCase {
+class DogPatchClient {
+  let baseURL: URL
+  let session: URLSession
+  let responseQueue: DispatchQueue?
   
-  func givenSUTFromJSON(fileName: String = "\(T.self)",
-    file: StaticString = #file,
-    line: UInt = #line) throws {
-    let decoder = JSONDecoder()
-    let data = try Data.fromJSON(fileName: fileName, file: file, line: line)
-    dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? NSDictionary
-    sut = try decoder.decode(T.self, from: data)
+  init(baseURL: URL, session: URLSession, responseQueue: DispatchQueue?) {
+    self.baseURL = baseURL
+    self.session = session
+    self.responseQueue = responseQueue
+  }
+  
+  func getDogs(completion: @escaping ([Dog]?, Error?) -> Void) -> URLSessionDataTask {
+    let url = URL(string: "dogs", relativeTo: baseURL)!
+    let task = session.dataTask(with: url) { data, response, error in
+      guard let response = response as? HTTPURLResponse, response.statusCode == 200, error == nil, let data = data else {
+        completion(nil, error)
+        return
+      }
+        let decoder = JSONDecoder()
+      do {
+        let dogs = try decoder.decode([Dog].self, from: data)
+        completion(dogs, nil)
+      } catch {
+        completion(nil, error)
+      }
+    }
+    task.resume()
+    return task
   }
 }
---
+++ DogPatch Chapter 8/DogPatchTests/Cases/Networking/DogPatchClientTests.swift
@@ -0,0 +1,206 @@
+/// Copyright (c) 2021 Razeware LLC
+/// 
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+/// 
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+/// 
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+/// 
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+@testable import DogPatch
+import XCTest
+
+class DogPatchClientTests: XCTestCase {
+  var sut: DogPatchClient!
+  var baseURL: URL!
+  var mockSession: MockURLSession!
+  var getDogsURL: URL {
+    URL(string: "dogs", relativeTo: baseURL)!
+  }
+  
+  override func setUp() {
+    super.setUp()
+    baseURL = URL(string: "https://example.com/api/v1/")!
+    mockSession = MockURLSession()
+    sut = DogPatchClient(baseURL: baseURL, session: mockSession, responseQueue: nil)
+  }
+  
+  override func tearDown() {
+    baseURL = nil
+    mockSession = nil
+    sut = nil
+    super.tearDown()
+  }
+  
+  func whenGetDogs(data: Data? = nil, statusCode: Int = 200, error: Error? = nil) -> (calledCompletion: Bool, dogs: [Dog]?, error: Error?) {
+    let response = HTTPURLResponse(url: getDogsURL, statusCode: statusCode, httpVersion: nil, headerFields: nil)
+    
+    var calledCompletion = false
+    var receivedDogs: [Dog]? = nil
+    var receivedError: Error? = nil
+    
+    let mockTask = sut.getDogs { dogs, error in
+      calledCompletion = true
+      receivedDogs = dogs
+      receivedError = error as NSError?
+    } as! MockURLSessionDataTask
+    
+    mockTask.completionHandler(data, response, error)
+    return (calledCompletion, receivedDogs, receivedError)
+    
+  }
+  
+  func test_init_sets_baseURL() {
+    XCTAssertEqual(sut.baseURL, baseURL)
+  }
+  
+  func test_init_sets_session() {
+    XCTAssertEqual(sut.session, mockSession)
+  }
+  
+  func test_getDogs_callsExpectedURL() {
+    // when
+    let mockTask = sut.getDogs() { _, _ in } as! MockURLSessionDataTask
+    
+    // then
+    XCTAssertEqual(mockTask.url, getDogsURL)
+  }
+  
+  func test_getDogs_callsResumeOnTask() {
+    // when
+    let mockTask = sut.getDogs { _, _ in } as! MockURLSessionDataTask
+    
+    // then
+    XCTAssertTrue(mockTask.calledResume)
+  }
+  
+  func test_getDogs_givenResponseStatusCode500_callsCompletion() {
+    // when
+    let result = whenGetDogs(statusCode: 500)
+    
+    // then
+    XCTAssertTrue(result.calledCompletion)
+    XCTAssertNil(result.dogs)
+    XCTAssertNil(result.error)
+  }
+  
+  func test_getDogs_givenError_callsCompletionWithError() throws {
+    // given
+    let expectedError = NSError(domain: "com.DogPatchTests", code: 42)
+    
+    // when
+    let result = whenGetDogs(error: expectedError)
+    
+    // then
+    XCTAssertTrue(result.calledCompletion)
+    XCTAssertNil(result.dogs)
+    
+    let actualError = try XCTUnwrap(result.error as NSError?)
+    XCTAssertEqual(actualError, expectedError)
+  }
+  
+  func test_getDogs_givenValidJSON_callsCompletionWithDogs() throws {
+    // given
+    let data = try Data.fromJSON(fileName: "GET_Dogs_Response")
+    let decoder = JSONDecoder()
+    let dogs = try decoder.decode([Dog].self, from: data)
+    
+    // when
+    let result = whenGetDogs(data: data)
+    
+    // then
+    XCTAssertTrue(result.calledCompletion)
+    XCTAssertEqual(result.dogs, dogs)
+    XCTAssertNil(result.error)
+  }
+  
+  func test_getDogs_givenInvalidJSON_callsCompletionWithError() throws {
+    // given
+    let data = try Data.fromJSON(fileName: "GET_Dogs_MissingValuesResponse")
+    
+    var expectedError: NSError!
+    let decoder = JSONDecoder()
+    do {
+      _ = try decoder.decode([Dog].self, from: data)
+    } catch {
+      expectedError = error as NSError
+    }
+    
+    // when
+    let result = whenGetDogs(data: data)
+    
+    // then
+    XCTAssertTrue(result.calledCompletion)
+    XCTAssertNil(result.dogs)
+    
+    let actualError = try XCTUnwrap(result.error as NSError?)
+    XCTAssertEqual(actualError.domain, expectedError.domain)
+    XCTAssertEqual(actualError.code, expectedError.code)
+  }
+  
+  func test_init_sets_responseQueue() {
+    // given
+    let responseQueue = DispatchQueue.main
+    
+    // when
+    sut = DogPatchClient(baseURL: baseURL, session: mockSession, responseQueue: responseQueue)
+    
+    // then
+    XCTAssertEqual(sut.responseQueue, responseQueue)
+  }
+}
+
+class MockURLSession: URLSession {
+  var queue: DispatchQueue? = nil
+  
+  func givenDispatchQueue() {
+    queue = DispatchQueue(label: "com.DogPatchTests.MockSession")
+  }
+  
+  override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+    MockURLSessionDataTask(completionHandler: completionHandler, url: url, queue: queue)
+  }
+}
+
+class MockURLSessionDataTask: URLSessionDataTask {
+  var completionHandler: (Data?, URLResponse?, Error?) -> Void
+  var url: URL
+  var calledResume = false
+  
+  init(completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void, url: URL, queue: DispatchQueue?) {
+    if let queue = queue {
+      self.completionHandler = { data, response, error in
+        queue.async {
+          completionHandler(data, response, error)
+        }
+      }
+    } else {
+      self.completionHandler = completionHandler
+    }
+    self.url = url
+    super.init()
+  }
+  
+  override func resume() {
+    calledResume = true
+  }
+}
